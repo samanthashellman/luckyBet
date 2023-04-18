0b0d6219111260bf3555e6ed00c520a73c8f473b
zionwashington <zion.washington@colorado.edu>
2023-04-18 14:37:52 -0600

    user updated, roulette started

--- Roulette.java
+++ Roulette.java
@@ -1,5 +1,35 @@
+import java.util.Scanner; 
 public class Roulette implements Command{
+
+    
+
+    private Boolean valueChip;
+    private Boolean nonValueChip;
+    private int valueChipAmount;
+    private int nonValueChipAmount;
+    private int totalChip;
+    private int buyInAmount;
+    private User currUser;
+
+    Roulette(User user) { 
+        nonValueChipAmount = 0;
+        valueChipAmount = 0;
+        this.currUser = user;
+    }
     public void execute(User user){
         System.out.println("Let's play Roulette!");
     };
+
+    public void setChip() {
+        System.out.print("Choose your buy-in:\n 1. Value Chip for 5$\n 2. Non-Value Chip");
+        Scanner input = new Scanner(System.in); 
+        int chip = input.nextInt();
+        if(chip == 1) {
+            System.out.print("Value chip it is! How many would you like? You currently have: " + valueChipAmount + " value chips and " + nonValueChipAmount + "non-value chips. For a total of: "  + totalChip + "chips. Making your buy in amount: " + buyInAmount + ".00.\n Remember, value chips are 5.00 each." );
+            int amountValue = input.nextInt();
+            
+
+        }
+    }
+
 }
--- User.java
+++ User.java
@@ -2,8 +2,37 @@
 public class User {
     
     String username;
-    int money;
+    int moneyWonRoulette;
+    int moneyWonFruit;
+    int moneyWonJur;
+    int moneyWonWorld;
+    int balance; //same as money?? i dunno.
+    String password;
     // track power-ups too
 
-    public User(){}
+    public User(){ }
+
+    public int getBalance() {
+        return balance;
+    }
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+    public void updateMoneyWonRoulette(int money) {
+        moneyWonRoulette += money;
+
+    }
+
+    public void updateMoneyWonFruit( int money ){
+        moneyWonFruit += money;
+
+    }
+    public void updateMoneyWonJur(int money) {
+        moneyWonJur += money;
+
+    }
+    public void updateMoneyWonWorld(int money) {
+        moneyWonWorld += money;
+
+    }
 }
